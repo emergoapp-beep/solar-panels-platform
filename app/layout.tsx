1dbb2b205feeebf3b700ff9fd462a77e25da8db9
emergoapp-beep <emergoapp@gmail.com>
2026-09-13 08:20:05 +0200

    Design: font titoli da Fredoka a Poppins (più professionale)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Fredoka } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import AppFrame from "@/components/layout/AppFrame";
 import AmbientBackground from "@/components/layout/AmbientBackground";
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const fredoka = Fredoka({
+const poppins = Poppins({
   variable: "--font-display",
   subsets: ["latin"],
   weight: ["600", "700"],
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html
       lang="it"
-      className={`${geistSans.variable} ${geistMono.variable} ${fredoka.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <AmbientBackground />
